97232c59ced6c5ea4018c0bf5722c6a442e2566b
Hemansh Solanki <solankihemansh@gmail.com>
2025-03-19 23:17:08 +0000

    Containerized movies

--- config/views.py
+++ config/views.py
@@ -6,8 +6,7 @@ def home(request):
     featured_movies = Movie.objects.order_by('-release_date')[:4]  # Fetch latest 4 movies
     featured_events = Event.objects.order_by('-date')[:4]  # Fetch latest 4 events
 
-    print("Movies in homepage:", featured_movies)
-    print("Events in homepage:", featured_events)
+   
 
     return render(request, 'home.html', {
         'featured_movies': featured_movies,

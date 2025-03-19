b42afca3e76aeadfc6743f998778a44b08e5787f
rohanathan.s@hotmail.com <rohanthan.s@hotmail.com>
2025-03-19 21:46:44 +0000

    Integrated Payment with Movies

--- apps/users/models.py
+++ apps/users/models.py
@@ -3,7 +3,12 @@ from django.db import models
 
 class CustomUser(AbstractUser):
     email = models.EmailField(unique=True)  # Ensure unique emails
-    profile_picture = models.ImageField(upload_to='profile_pics/', null=True, blank=True)
+    profile_picture = models.ImageField(
+         upload_to='profile_pics/', 
+         null=True, 
+         blank=True, 
+         default='profile_pics/default.png'  # Set a default image
+     )
     phone_number = models.CharField(max_length=15, null=True, blank=True)
     date_of_birth = models.DateField(null=True, blank=True)
 
